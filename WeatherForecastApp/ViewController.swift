fa86f4f3b1f8fd034bdbedf2ef2691292e1b1ec6
Gustavo Angulo <gustavo.p.angulo@gmail.com>
2025-02-02 17:24:28 -0300

    Initial Commit

---
+++ WeatherForecastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecastApp
+//
+//  Created by Gustavo Angulo on 02/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherForecastAppTests/WeatherForecastAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastAppTests.swift
+//  WeatherForecastAppTests
+//
+//  Created by Gustavo Angulo on 02/02/25.
+//
+
+import Testing
+@testable import WeatherForecastApp
+
+struct WeatherForecastAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
